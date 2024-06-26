35bd60694b1095ed5dc7442f1e3dfeeabe0bc6ce
shept <sheptikhinv@gmail.com>
2024-06-26 13:12:03 +0500

    little changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
+import styles from "./Header.module.css";
+
 const Header = () => {
     return (
-        <>
-        </>
+        <header>
+            <h1 className={styles.name}>slavagm</h1>
+        </header>
     );
 };
 
